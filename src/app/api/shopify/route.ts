555d4a0b2658567670e194223cb5bc7cc2fc35b3
info@nicevois.com <info@nicevois.com>
2025-03-13 10:25:32 +0700

    fix> grammar logic

--- src/app/api/shopify/route.ts
+++ src/app/api/shopify/route.ts
@@ -282,7 +282,7 @@ export async function POST(request: NextRequest) {
         if (songUrl) _customAttributes.push({ key: "* Song URL", value: songUrl });
         _customAttributes.push({
             key: "* Lyrics Change",
-            value: `\n(Word changes: ${wordChanged} word${wordChanged !== 1 ? 's' : ''})\n${formattedLyricsChanges}`
+            value: `\n(Word changes: ${wordChanged} word${wordChanged > 1 ? 's' : ''})\n${formattedLyricsChanges}`
         });
         if (specialRequests) _customAttributes.push({ key: "* Special Requests", value: `"${specialRequests}"` });
         _customAttributes.push({
@@ -295,12 +295,12 @@ export async function POST(request: NextRequest) {
             lineItems: [{
                 quantity: 1,
                 title: "Change Song Lyrics Service | Nicevois.com",
-                originalUnitPrice: "0.00",//String(price.toFixed(2)), // Standardized format
+                originalUnitPrice: String(price.toFixed(2)), // Standardized format
                 customAttributes: _customAttributes,
                 taxable: false
             }],
             // customAttributes,
-            // note: `Lyrics change:\n(${wordChanged} word${wordChanged !== 1 ? 's' : ''})\n${formattedLyricsChanges}`,
+            // note: `Lyrics change:\n(${wordChanged} word${wordChanged > 1 ? 's' : ''})\n${formattedLyricsChanges}`,
             tags: [`${deliveryType}-delivery`, "custom-lyrics"],
             shippingLine: {
                 title: "Digital Delivery",
