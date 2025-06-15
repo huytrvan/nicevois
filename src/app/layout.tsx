14eb64094913c25b1e70ef6843dfdc761a1efd45
info@nicevois.com <info@nicevois.com>
2025-06-15 14:17:50 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout({
                         __html: `
                             (function() {
                                 const allowedOrigins = ${JSON.stringify(SHOP_ORIGINS)};
-                                const isInIframe = window.self !== window.top;
-                                
-                                if (isInIframe) {
                                     const isLocalhost = window.location.hostname === 'localhost' ||
                                         window.location.hostname === '127.0.0.1'
                                     
@@ -65,7 +62,6 @@ export default function RootLayout({
                                             return;
                                         }
                                     }
-                                }
                             })();
                         `,
                     }}
