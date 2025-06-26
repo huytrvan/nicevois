717b2714401da74c18ff8712e49a7248bce12a79
Huy <huy>
2025-06-26 15:03:36 +0700

    test no expand

--- src/components/ViewportToaster.tsx
+++ src/components/ViewportToaster.tsx
@@ -272,7 +272,7 @@ export default function ViewportToaster() {
     return (
         <Toaster
             position="top-center"
-            expand={true}
+            // expand={true}
             richColors
             // closeButton
             toastOptions={toastOptions}
